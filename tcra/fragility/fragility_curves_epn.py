9b0f81f218e72fef92e7600b3ef0a53283f2c118
rxm562 <51394123+rxm562@users.noreply.github.com>
2024-06-12 00:35:57 -0400

    Update fragility_curves_epn.py

--- tcra/fragility/fragility_curves_epn.py
+++ tcra/fragility/fragility_curves_epn.py
@@ -1,3 +1,10 @@
+"""
+The tsnet.simulation.main module contains function to perform
+the workflow of read, discretize, initial, and transient
+simulation for the given .inp file.
+
+"""
+
 fragility_curves_epn = {
     'PW': {
         'Fail': {'mu': 130.139, 'sigma': 0.1213}
