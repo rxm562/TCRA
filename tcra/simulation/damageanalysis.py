07abf9d951b625f15e1669e392dbc9d2476a02a5
rxm562 <51394123+rxm562@users.noreply.github.com>
2024-06-12 00:09:51 -0400

    Update damageanalysis.py

--- tcra/simulation/damageanalysis.py
+++ tcra/simulation/damageanalysis.py
@@ -1,3 +1,11 @@
+"""
+The tsnet.network.geometry read in the geometry defined by EPANet
+.inp file, and assign additional parameters needed in transient
+simulation later in tsnet.
+
+"""
+
+
 import numpy as np
 import pandas as pd
 from scipy.stats import lognorm
