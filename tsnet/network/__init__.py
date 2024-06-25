fccc8cfaf8d5b0fc314fa0e59164d523027e558f
rxm562 <51394123+rxm562@users.noreply.github.com>
2024-06-25 15:53:22 -0400

    Create __init__.py

---
+++ tsnet/network/__init__.py
@@ -0,0 +1,11 @@
+"""
+The tsnet.network package contains methods to define
+1. a water network geometry,
+2. network topology,
+3. network control, and
+4 .spatial and temporal discretization.
+
+"""
+
+from .topology import topology
+from .model import TransientModel
