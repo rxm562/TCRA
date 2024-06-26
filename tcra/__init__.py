712c6e81fac8ed4a6156ef6c7d064b141cf6dffc
rxm562 <51394123+rxm562@users.noreply.github.com>
2024-06-25 20:43:01 -0400

    Update __init__.py

--- tcra/__init__.py
+++ tcra/__init__.py
@@ -1,10 +1,4 @@
 from tcra import fragility
 from tcra import simulation
 
-__version__ = '0.1.2'
-
-__copyright__ = """Copyright 2015 Sandia Corporation.
-Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
-the U.S. Government retains certain rights in this software."""
-
-__license__ = "Revised BSD License"
+__version__ = '0.1'
