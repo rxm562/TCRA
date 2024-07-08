18cf8389d68c737b4ed4a1f27824ae14e7fdc50b
rxm562 <51394123+rxm562@users.noreply.github.com>
2024-07-07 23:30:25 -0400

    Update __init__.py

--- tcra/__init__.py
+++ tcra/__init__.py
@@ -1,4 +1,9 @@
+"""Top-level package for TCRA."""
+
 from tcra import simulation
 from tcra import fragility
 
+__author__ = """Ram Krishna Mazumder"""
+__email__ = 'rxm562@case.edu'
 __version__ = '0.1'
+
