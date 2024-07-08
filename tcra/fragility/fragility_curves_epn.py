ad5abc10ac405359023032078efd8ce8dd99cd9d
rxm562 <51394123+rxm562@users.noreply.github.com>
2024-07-07 23:16:42 -0400

    Update fragility_curves_epn.py

--- tcra/fragility/fragility_curves_epn.py
+++ tcra/fragility/fragility_curves_epn.py
@@ -5,6 +5,18 @@ simulation for the given .inp file.
 
 """
 
+
+        """Calculate the probability of failure given a sample of damage states.
+        Parameters
+        ----------
+        ki : float or int or list, optional
+            If given as float or int, set the value as wavespeed
+            for all pipe; If given as list set the corresponding
+            value to each pipe, by default 1200.
+        dt : str or list, optional
+            The list of pipe to define wavespeed,
+            by default all pipe in the network.
+        """
 fragility_curves_epn = {
     'PW': {
         'Fail': {'mu': 130.139, 'sigma': 0.1213}
