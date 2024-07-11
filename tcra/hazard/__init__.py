41a2a1788419cdcdb1fae477616f5d660c15864e
rxm562 <51394123+rxm562@users.noreply.github.com>
2024-07-10 23:39:01 -0400

    Create __init__.py

---
+++ tcra/hazard/__init__.py
@@ -0,0 +1,4 @@
+"""Hazard Analysis module contains functions to estimate cyclone parameters and gust wind speeds."""
+
+from tcra.CycloneParameters import estimate_parameters
+from tcra.CycloneParameters import calculate_wind_speeds
