836cde6a62ea6c8d94a3b6fc5443d073eb689bc3
rxm562 <51394123+rxm562@users.noreply.github.com>
2024-07-10 23:43:10 -0400

    Create __init__.py

---
+++ tcra/damageprob/__init__.py
@@ -0,0 +1,5 @@
+"""Damage Probability Analysis module contains functions to estimate probability of failures."""
+
+from tcra.DamageProbability import DamageProbabilityCalculator
+from tcra.DamageProbability.DamageProbabilityCalculator import calc_probability_failure_value
+from tcra.DamageProbability.DamageProbabilityCalculator import sample_damage_interval
